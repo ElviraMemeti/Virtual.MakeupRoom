f562a64b1225a0d29a520e6512f3e8f51a1986a5
Eronita Emini <eronitaemini@gmail.com>
2024-02-19 16:59:43 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
+
+
+import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from 'three/addons/lights/RectAreaLightUniformsLib.js';
  // Scene, Camera and Renderer
 const scene = new THREE.Scene();
-
+scene.background = new THREE.Color( 0xfff0ff );
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 15;
+camera.position.y = 10;
+camera.position.x= 5;
 
-const renderer = new THREE.WebGLRenderer();
+
+const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
@@ -22,24 +29,82 @@ scene.add(axesHelper);
 // Groups
 const roomAndFurnitureGroup = new THREE.Group();
 
+const geometry=new THREE.BoxGeometry(6,1.5,0.1,10,5,10);
+const material=new THREE.MeshStandardMaterial({color:0xDC143C, roughness:0.1, metalness:0});
+const rectangle=new THREE.Mesh(geometry,material);
+rectangle.position.set(-2, 1.5, -9.4);
+scene.add(rectangle);
+
+RectAreaLightUniformsLib.init();
+const rectLight1 = new THREE.RectAreaLight( 0xff0000, 5, 6, 10 );
+rectLight1.position.copy(rectangle.position);
+rectLight1.lookAt(-1,1,100);
+scene.add( rectLight1 );
+
 
 //Models
+
 const loader = new GLTFLoader();
-loader.load('models/room/room.gltf', (gltf) => {
-    const room = gltf.scene;
-    console.log('Model loaded:', room);
+const textureLoader = new THREE.TextureLoader();
+const wallTexture=textureLoader.load('/textures/wallTexture2.jpg');
+textureLoader.load('/textures/woodflor.avif', function (texture) {
+    loader.load('models/room/room.gltf', (gltf) => {
+      const room = gltf.scene;
+      console.log('Model loaded:', room);
 
       room.traverse((child) => {
+        // console.log(child.name);
         if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
+          child.castShadow = true;
+          child.receiveShadow = true;
+  
+          if (child.name === 'Object_56') {
+            console.log('Floor found');
+            child.material = new THREE.MeshPhongMaterial({ map: texture, shininess:1000});
+            texture.wrapS = THREE.MirroredRepeatWrapping;
+            texture.wrapT = THREE.MirroredRepeatWrapping;
+            texture.repeat.set( 1, 1 );
+
+          } else {
+            console.log('Floor not found');
+           
+          }
+
+          if(child.name=='Object_3'){
+            console.log('Wall found');
+            child.material=new THREE.MeshStandardMaterial({color:0xFFFFFF});
+
+                }
+                }
+            });
+        scene.add(room);
+    }, undefined, function (error) {
+      console.error('An error happened:', error);
     });
+  }, undefined, function (error) {
+    console.log('An error occurred while loading the texture:', error);
+  });
 
-    room.scale.set(1, 1, 1);
-    roomAndFurnitureGroup.add(room);
+  const planeGeometry = new THREE.PlaneGeometry(10, 20); 
+  const planeMaterial = new THREE.MeshStandardMaterial({ 
+  color: 0xffff00, 
+  side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -Math.PI / 2;
+plane.scale.set(1.55, 0.9, 1.2);
 
-    scene.add(room);
+plane.position.set(0.3, 2.71, -1);
+
+// scene.add(plane);
+// roomAndFurnitureGroup.add(planeGeometry);
+
+loader.load('models/room/.ceilingLight.gltf', (gltf) => {
+  const cielingLamp = gltf.scene;
+   cielingLamp.scale.set(1, 0.4, 1.2);
+ // longTable.rotation.y = (Math.PI/2);
+  cielingLamp.position.set(-6.5, 2, -3)
+  roomAndFurnitureGroup.add(cielingLamp); 
 });
 
 
@@ -53,35 +118,167 @@ loader.load('models/longTable/longTable.gltf', (gltf) => {
 
 
 
-loader.load('models/dressingTable/dressingTable.gltf', (gltf) => {
-    const dressingTable = gltf.scene;
-    dressingTable.scale.set(1.5, 1.5, 1.5);
-   dressingTable.rotation.y = (Math.PI/2);
-   dressingTable.position.set(6.7, 0, -4.5)
-    roomAndFurnitureGroup.add(dressingTable); 
+// loader.load('models/dressingTable/dressingTable.gltf', (gltf) => {
+//     const dressingTable = gltf.scene;
+//     dressingTable.scale.set(1.5, 1.5, 1.5);
+//    dressingTable.rotation.y = (Math.PI/2);
+//    dressingTable.position.set(6.7, 0, -4.5)
+//     roomAndFurnitureGroup.add(dressingTable); 
+// });
+
+
+// loader.load('models/dressingTable/dressingTable.gltf', (gltf) => {
+//     const dressingTable = gltf.scene;
+//     dressingTable.scale.set(1.5, 1.5, 1.5);
+//    dressingTable.rotation.y = (Math.PI/2);
+//    dressingTable.position.set(6.7, 0, -1.5)
+//     roomAndFurnitureGroup.add(dressingTable); 
+// });
+
+
+// loader.load('models/dressingTable/dressingTable.gltf', (gltf) => {
+//     const dressingTable = gltf.scene;
+//     dressingTable.scale.set(1.5, 1.5, 1.5);
+//    dressingTable.rotation.y = (Math.PI/2);
+//    dressingTable.position.set(6.7, 0, 1.5)
+//     roomAndFurnitureGroup.add(dressingTable); 
+// });
+
+
+let woodenTexture = new THREE.TextureLoader();
+ woodenTexture=textureLoader.load('/textures/woodTexture.jpg');
+
+loader.load('models/mirror/makeupmirror.gltf', (gltf) => {
+    const longmirror = gltf.scene;
+    longmirror.scale.set(1.3, 0.92, 3);
+   longmirror.rotation.y = (Math.PI/-2);
+  //  longmirror.rotation.x = (Math.PI/);
+
+   longmirror.position.set(7.74, 1.3, -3)
+    roomAndFurnitureGroup.add(longmirror); 
+
+    longmirror.traverse((child)=>{
+      if(child.isMesh && child.name=='Listones'){
+      
+        console.log('wood found');
+        child.material=new THREE.MeshStandardMaterial({color:0x000000});
+
+      } else {
+        console.log("wood not found");
+      }
+    });
+    
 });
 
 
-loader.load('models/dressingTable/dressingTable.gltf', (gltf) => {
-    const dressingTable = gltf.scene;
-    dressingTable.scale.set(1.5, 1.5, 1.5);
-   dressingTable.rotation.y = (Math.PI/2);
-   dressingTable.position.set(6.7, 0, -1.5)
-    roomAndFurnitureGroup.add(dressingTable); 
+loader.load('models/mirror/makeupmirror.gltf', (gltf) => {
+  const longmirror = gltf.scene;
+  longmirror.scale.set(1.3, 0.92, 3);
+ longmirror.rotation.y = (Math.PI/-2);
+ longmirror.traverse((child)=>{
+  if(child.isMesh && child.name=='Listones'){
+  
+    console.log('wood found');
+    child.material=new THREE.MeshStandardMaterial({color:0x000000});
+
+  } else {
+    console.log("wood not found");
+  }
+});
+ longmirror.position.set(7.74, 1.3, 3)
+  roomAndFurnitureGroup.add(longmirror); 
 });
 
+loader.load('models/room/table.gltf', (gltf) => {
+  const table = gltf.scene;
+  table.scale.set(1.5, 1.5, 1.5);
+ table.rotation.y = (Math.PI/-2);
 
-loader.load('models/dressingTable/dressingTable.gltf', (gltf) => {
-    const dressingTable = gltf.scene;
-    dressingTable.scale.set(1.5, 1.5, 1.5);
-   dressingTable.rotation.y = (Math.PI/2);
-   dressingTable.position.set(6.7, 0, 1.5)
-    roomAndFurnitureGroup.add(dressingTable); 
+ table.position.set(-1.5, 0, -5)
+  roomAndFurnitureGroup.add(table); 
 });
 
+const plantsGroup=new THREE.Group();
 
+//plants
+loader.load('models/plants/aloePlant/scene.gltf', (gltf) => {
+    const aloeplant = gltf.scene;
+    aloeplant.scale.set(0.2, 0.2, 0.2 );
+    aloeplant.position.set(-5, 0, -6)
+    scene.add(aloeplant); 
+});
+loader.load('models/plants/pot.gltf', (gltf) => {
+    const flaskfilondreon = gltf.scene;
+    flaskfilondreon.scale.set(0.3, 0.3, 0.3 );
+    flaskfilondreon.position.set(-4.2, 0, -6.5)
+    scene.add(flaskfilondreon); 
+});
 
+loader.load('models/couch/SofaBaker.gltf', (gltf) => {
+    const couch = gltf.scene;
+    couch.scale.set(2.5, 2, 2 );
+    couch.position.set(-1, 0, -7)
+    scene.add(couch); 
 
+    couch.traverse(function (child) {
+        if (child.isMesh) {
+           child.material=new THREE.MeshStandardMaterial({map:BeanBagTexture});
+        }
+    }); 
+});
+
+const bbTextureLoader = new THREE.TextureLoader();
+const BeanBagTexture=bbTextureLoader.load('/textures/beanBagTexture.jpg');
+
+loader.load('models/couch/BeanBag.gltf', (gltf) => {
+    const beanBag = gltf.scene;
+    beanBag.scale.set(2.5, 2, 2 );
+    beanBag.position.set(-3, 0, -4)
+    scene.add(beanBag); 
+    beanBag.rotation.y = (Math.PI/1.5);
+    
+
+    beanBag.traverse(function (child) {
+        if (child.isMesh) {
+            child.material = new THREE.MeshStandardMaterial({ map: BeanBagTexture });
+            
+        }
+    }); 
+
+});
+
+loader.load('models/lamps/lamp.gltf', (gltf) => {
+    const lamp = gltf.scene;
+    lamp.scale.set(1.5, 1.5, 1.5);
+    lamp.position.set(6.5, 0, 5)
+    lamp.rotation.y = (Math.PI/3);
+  
+    scene.add(lamp); 
+  });
+  const spotLight=new THREE.SpotLight(0xFFFFFF,10,2,5,5);
+  spotLight.position.set(5,1.5,5);
+  // spotLight.rotation.y = (Math.PI/);
+  spotLight.lookAt(0,0,0);
+  scene.add(spotLight);
+  const spotLightHelper=new THREE.SpotLightHelper(spotLight);
+  // scene.add(spotLightHelper);
+  scene.add(roomAndFurnitureGroup);
+  
+  
+  const windowLight=new THREE.DirectionalLight(0xFFFFFF,2);
+  windowLight.position.set(0, 10, 50);
+  windowLight.target.position.set(0, 0, 0); 
+  windowLight.castShadow = true;
+  windowLight.shadow.mapSize.width = 2048; 
+  windowLight.shadow.mapSize.height = 2048; 
+  windowLight.shadow.camera.left = -30;
+  windowLight.shadow.camera.right = 30;
+  windowLight.shadow.camera.top = 30;
+  windowLight.shadow.camera.bottom = -30;
+  windowLight.shadow.camera.near = 1;
+  windowLight.shadow.camera.far = 100; 
+  
+  scene.add(windowLight);
 
 loader.load('models/letters/letters.gltf', (gltf) => {
     const letters = gltf.scene;
@@ -237,21 +434,12 @@ oppositePointLight.position.set(8, 20, 5); // Adjust the position of the point l
 oppositePointLight.castShadow = true;
 scene.add(oppositePointLight);
 
-
-
-
-
-
 // OrbitControls for rotation
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
-
-
-
-
 
 // Add event listener for keyboard input
 document.addEventListener('keydown', (event) => {
@@ -277,11 +465,6 @@ document.addEventListener('keydown', (event) => {
             break;
     }
 });
-
-
-
-
-
 
 
 const animate = () => {
